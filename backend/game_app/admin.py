c653e57e915fccdf42ada22e48497eed98f69196
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-08-25 03:50:22 +0330

    Modified games admin, models to include cover images for each game seperated by its own dir

--- backend/game_app/admin.py
+++ backend/game_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Game, Genre
 # Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_genres')
+    filter_horizontal = ('genres',)
+    
+    def display_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+    
+    display_genres.short_description = 'Genres'
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Genre)
--- backend/game_app/models.py
+++ backend/game_app/models.py
@@ -1,5 +1,23 @@
-from django.db import models
+import os
 
+from django.db import models
+from django.utils.text import slugify
+
+# Util function
+def game_upload_path(instance, filename):
+    # Debugging statements to check values
+    print(f"Title: {instance.title}, Filename: {filename}")
+    
+    if not instance.title:
+        raise ValueError("The game must have a title before uploading an image.")
+    
+    sanitized_title = slugify(instance.title) if instance.title else 'untitle-game'
+    
+    if not filename:
+        raise ValueError("The file must have a valid name.")
+    
+    return os.path.join('games', sanitized_title, filename)
+    
 class Genre(models.Model):
     name = models.CharField(max_length=255)
     
@@ -8,7 +26,8 @@ class Genre(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=255)
-    genre = models.ManyToManyField(Genre)
+    genres = models.ManyToManyField(Genre)
+    cover = models.ImageField(upload_to=game_upload_path, blank=True, null=True)
     
     def __str__(self) -> str:
         return self.title
